f83b6e515d6eeee7e01e308221f10efff8cb44c3
adam-ghavi <adamghaviyasha@gmail.com>
2026-03-10 14:16:21 +0700

    [RED] Created tests for PaymentService

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceTest.java
@@ -0,0 +1,87 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentServiceTest {
+    PaymentService paymentService;
+    Order order;
+    Map<String, String> paymentData;
+
+    @BeforeEach
+    void setUp() {
+        paymentService = new PaymentServiceImpl(new PaymentRepository());
+
+        List<Product> products = new ArrayList<>();
+        Product product = new Product();
+        product.setProductId("prod-1");
+        product.setProductName("Sabun Cap Usep");
+        product.setProductQuantity(1);
+        products.add(product);
+
+        order = new Order(
+                "order-1",
+                products,
+                1708560000L,
+                "Safira Sudrajat"
+        );
+
+        paymentData = new HashMap<>();
+        paymentData.put("bank", "BCA");
+        paymentData.put("accountNumber", "1234567890");
+    }
+
+    @Test
+    void testAddPayment() {
+        Payment payment = paymentService.addPayment(order, "BANK_TRANSFER", paymentData);
+
+        assertNotNull(payment);
+        assertEquals("BANK_TRANSFER", payment.getMethod());
+        assertEquals(paymentData, payment.getPaymentData());
+    }
+
+    @Test
+    void testGetPaymentById() {
+        Payment payment = paymentService.addPayment(order, "BANK_TRANSFER", paymentData);
+        Payment foundPayment = paymentService.getPayment(payment.getId());
+
+        assertNotNull(foundPayment);
+        assertEquals(payment.getId(), foundPayment.getId());
+    }
+
+    @Test
+    void testGetAllPayments() {
+        paymentService.addPayment(order, "BANK_TRANSFER", paymentData);
+        paymentService.addPayment(order, "EWALLET", paymentData);
+
+        List<Payment> payments = paymentService.getAllPayments();
+
+        assertEquals(2, payments.size());
+    }
+
+    @Test
+    void testSetStatusSuccessAlsoUpdatesOrder() {
+        Payment payment = paymentService.addPayment(order, "BANK_TRANSFER", paymentData);
+
+        paymentService.setStatus(payment, "SUCCESS");
+
+        assertEquals("SUCCESS", payment.getStatus());
+        assertEquals("SUCCESS", order.getStatus());
+    }
+
+    @Test
+    void testSetStatusRejectedAlsoUpdatesOrder() {
+        Payment payment = paymentService.addPayment(order, "BANK_TRANSFER", paymentData);
+
+        paymentService.setStatus(payment, "REJECTED");
+
+        assertEquals("REJECTED", payment.getStatus());
+        assertEquals("FAILED", order.getStatus());
+    }
+}
